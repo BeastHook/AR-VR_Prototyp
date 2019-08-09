2da679e3e344eb43c7bc54d59cc66e79991f0646
Luca Hillen <luca_hillen@yahoo.de>
2019-08-09 15:47:55 +0200

    First AR Visual Prototype
    Ohne große Funktion aber schon mal was fürs Auge

---
+++ Assets/Scripts/createCard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createCard : MonoBehaviour
+{
+
+    public bool isEnabled = false;
+    public GameObject card;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        card.SetActive(isEnabled);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        card.SetActive(isEnabled);
+
+    }
+}
